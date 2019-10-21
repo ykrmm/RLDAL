a5f86277bc5064369ed6b430fb2759221e07a537
4bach <yanniskarmim@hotmail.fr>
2019-10-21 18:15:14 +0200

    dqn

--- TME/DQN_CartPole_LundarLander/Agents/dqnAgentCartpole.py
+++ TME/DQN_CartPole_LundarLander/Agents/dqnAgentCartpole.py
@@ -27,42 +27,70 @@ class NN_Q(torch.nn.Module):
         return x
 
 class DQN_Agent(object):
-    def __init__(self,env,learning_rate,discount,inSize,batchsize,layers=[],loss=nn.SmoothL1Loss(),epsilon=0.1,capacity = 1000,c_step = 20):
+    def __init__(self,env,learning_rate,discount,batchsize,layers=[],loss=nn.SmoothL1Loss(),epsilon=0.1,capacity = 1000,c_step = 20):
         action_space=env.action_space
         self.env=env
         self.action_space = action_space # right or left
-        self.state = env.states
-        self.Q = np.zeros((len(self.state),self.action_space.n))
+        #self.state = env.states
+        #self.Q = np.zeros((len(self.state),self.action_space.n))
         self.epsilon = epsilon
         self.learning_rate = learning_rate
         self.batchsize = batchsize
         self.discount = discount
         self.lastobs = None 
         self.lasta = None
-        self.model = NN_Q(inSize,outSize=self.action_space.n,layers=layers)
+        self.Q = NN_Q(4,outSize=self.action_space.n,layers=layers)
         self.loss = loss
-        self.optim = torch.optim.Adam(self.model.parameters(),lr=self.learning_rate)
+        self.optim = torch.optim.Adam(self.Q.parameters(),lr=self.learning_rate)
         
         self.c_step = c_step # Step before Q_hat = Q 
         self.D = deque(maxlen=capacity) # transition history 
-        self.Q_hat = None
-
+        self.Q_hat = NN_Q(4,outSize=self.action_space.n,layers=layers) # pour éviter les maj pdt le backward 
+        self.Q_hat.load_state_dict(self.Q.state_dict())
+        self.count=0
     def act(self, observation, reward, done):
-        
-        tmp=self.env.state2str(observation)
-        self.obs = self.env.states[tmp]
+        self.obs=observation
+        #tmp=self.env.state2str(observation)
+        #self.obs = self.env.states[tmp]
         self.reward = reward
         if self.lasta is not None: # On entraine pas si c'est on est à la première étape
             self.D.append((self.lastobs,self.lasta,self.reward,self.obs,done))
             els = np.random.choice(range(len(self.D)),self.batchsize)
             l = [self.D[i] for i in els]
-            lso,la,ls1,lr,ld = map(list,zip(*l))
+            #print(l[0])
+            lso,la,lr,ls1,ld = map(list,zip(*l))
+            #print(ls1)
+            lr_asT=torch.Tensor(lr)
+            ld_asT=torch.BoolTensor(ld)
+            lso_asT=torch.Tensor(lso)
+            ls1_asT=torch.Tensor(ls1)
+            print(ld_asT.shape)
+            with torch.no_grad():
+                print("ls1",ls1_asT.shape)
+                q=self.Q_hat(ls1_asT)
+                val,ind=torch.max(q[range(self.batchsize),la],0)
+            
+                ypred=torch.where(ld_asT,lr_asT,lr_asT+self.discount*val)
+
+            print("ypred",ypred.shape)
+            
+            ytar=self.Q(lso_asT)
+            val,ind=torch.max(ytar[range(self.batchsize),la],0)
+            print("ytar",ytar.shape)
+            loss=self.loss(ypred,val)
+            loss.backward()
+            self.optim.step()
+            self.optim.zero_grad()
             # Entrainement neurone 
                 # entraine
             #
 
         if np.random.random() < 1 - self.epsilon and self.lasta is not None: # Sinon au debut ça bug pour la première action
-            _,action = torch.max(self.model(self.obs)) 
+            tmp=torch.from_numpy(self.obs)
+            print("tmp",tmp.shape)
+            tmp=self.Q(tmp.float())
+            action = torch.max(tmp) #marche pas ça
+            print(action)
             
         else : # action random
             action = np.random.randint(self.action_space.n) 
@@ -70,6 +98,11 @@ class DQN_Agent(object):
         
         self.lastobs = self.obs 
         self.lasta = action
+        self.count+=1
+        if self.count==self.c_step:
+            self.count=0
+            self.Q_hat.load_state_dict(self.Q.state_dict())
+
         return action
 
 
@@ -80,7 +113,7 @@ if __name__ == '__main__':
     env = gym.make('CartPole-v1')
 
     # Enregistrement de l'Agent
-    #agent = #DQN_Agent(env.action_space)
+    agent = DQN_Agent(env,learning_rate=0.01,discount=0.9,batchsize=20,layers=[12,8])
 
     outdir = 'cartpole-v0/random-agent-results'
     envm = wrappers.Monitor(env, directory=outdir, force=True, video_callable=False)
